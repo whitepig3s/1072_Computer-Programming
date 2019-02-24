bbcc8fe689943e9c6aed909d8ef60eb0ff9b6a55
whitepig3s <whitepig3s@gmail.com>
2019-02-24 17:50:26 +0800

    modify shuffle

--- poker/main.cpp
+++ poker/main.cpp
@@ -8,56 +8,69 @@ void InitPoker (int card_I [],int n)
         card_I[i]=n;
 }
 
-void Shuffle(int card_f [],int n)
+void Shuffle(int card_I [],int *card_f,int n) //洗牌
 {
-    rand 取一個位置
-    位置--
-    放到動態記憶體中
-    直到變成0
+    for (int i=0;i<n;i++)
+    {
+        int tmp;
+        do
+        {
+          tmp=rand()%52;
+
+        }while(card_I [tmp]!=0);
+        card_I [tmp]--;
+        card_f[i]=tmp;
+    }
+}
+
+/*ShowAll(card,n,k)
+{
 
 
+
+}*/
+
+void ShowOne (int n)
+{
+    if (n<=0&&n<=12)
+    {
+        cout<<"黑桃"<<n+1;
+    }
+    else if(n<=13&&n<=25)
+    {
+         cout<<"紅心"<<n-12;
+    }
+    else if(n<=26&&n<=38)
+    {
+         cout<<"紅磚"<<n-24;
+    }
+    else if(n<=39&&n<=51)
+    {
+         cout<<"梅花"<<n-36;
+    }
 }
 
 int main()
 {
     srand(time(0));
     int card[52]={0};
-    cout<<"Please enter the number of poker: "
+    cout<<"Please enter the number of poker: ";
     int input=0;    //poker 數量
     cin>>input;
-    InitPoker(card,n);
+    int cardmount=input*52;
+    InitPoker(card,input); //初始化 (確定有幾副牌)
 
-    for (int i=1; i<52; i++)
+
+    int *card_ptr;
+    card_ptr=new int[cardmount];
+    Shuffle(card,card_ptr,cardmount);
+
+    for (int i=1; i<cardmount; i++)
     {
-        cout<<card[i]<<" ";
+        cout<<card_ptr[i]<<" ";
     }
 
-    //新增動態記憶體陣列
-    Shuffle()
-
-
-
-
-    /*cout<<"after"<<endl;
-    for (int i =0;i<=500;i++)
-    {
-        int a1=randnbi()+1,a2=randnbj()+1,b1=randnbi()+1,b2=randnbj()+1;
-
-        int tmp=0;
-        tmp=p[a1][a2];
-        p[a1][a2]=p[b1][b2];
-        p[b1][b2]=tmp;
-
-    }
-        for (int i=1; i<=4; i++)
-    {
-        for(int j=1; j<14; j++)
-        {
-            cout<<p[i][j]<<" ";
-        }
-        cout<<endl;
-    }*/
-
-
+    delete[] card_ptr;
+    card_ptr =NULL;
     return 0;
 }
